e9ffafe0c32c4a7f22ad67f37e62db56d70b7fe5
Your Name <you@example.com>
2023-05-10 10:52:02 +0200

    debug deplassement(limite a la piece 10)

--- template/fragment/button_avancer.php
+++ template/fragment/button_avancer.php
@@ -1,1 +1,1 @@
-<button onclick="execute('deplacement','personnage', 'Nsalle=<?= $salle+1 ?>')">Avancer à la Pièce N°<?= $salle+1 ?></button>
+<button onclick="execute('deplacement','personnage', 'Nsalle=<?= $salle + 1 ?>')">Avancer à la Pièce N°<?= $salle + 1 ?></button>
--- template/fragment/personnage.php
+++ template/fragment/personnage.php
@@ -1,19 +1,20 @@
-
 <div class="info-perso">
 
     <h2><?= $objet->html("pseudo") ?></h2>
     <p>HP : <?= $objet->html("hp") ?></p>
     <p>Agilitée : <?= $objet->html("agilite") ?></p>
-    <p>Force : <?= $objet->html("force") ?>  </p>  <button onclick="execute('convertir_point','personnage','attribut=force')">Convertir 1 unitée en Résistance</button>
+    <p>Force : <?= $objet->html("force") ?> </p> <button onclick="execute('convertir_point','personnage','attribut=force')">Convertir 1 unitée en Résistance</button>
     <p>Résistance : <?= $objet->html("resistance") ?> </p> <button onclick="execute('convertir_point','personnage','attribut=resistance')">Convertir 1 unitée en Force</button>
-    
+
 </div>
 
 <h2>Pièce N° <?= $objet->html("salle") ?></h2>
-<?php  include "template/fragment/button_reculer.php" ?>
-<?php  include "template/fragment/button_avancer.php" ?>
+<?php $objet->get("salle") <= 1  ? "" : include "template/fragment/button_reculer.php" ?>
+<?php $objet->get("salle") >= 10 ? "" : include "template/fragment/button_avancer.php" ?>
 <table style="margin-top: 15px;  padding: 20px;">
-    <?= empty($listeAdversaires)?"<p> Vous êtes seul dans cette salle ... <p>":"" ?>
-    <?php  foreach( $listeAdversaires as $adversaire){  include "template/fragment/liste_adversaire.php";} ?>
+    <?= empty($listeAdversaires) ? "<p> Vous êtes seul dans cette salle ... <p>" : "" ?>
+    <?php foreach ($listeAdversaires as $adversaire) {
+        include "template/fragment/liste_adversaire.php";
+    } ?>
 </table>
 </div>
